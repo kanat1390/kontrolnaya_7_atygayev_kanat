11454f116b37bba1df0b9041f8d56e95f8750c92
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-01 11:36:41 +0600

    Создана модель Record которая представляет собой гостевую запись

--- src/records/models.py
+++ src/records/models.py
@@ -1,3 +1,19 @@
+from random import choices
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('active', 'Активно'), 
+    ('blocked', 'Заблокировано'), 
+)
+class Record(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=100)
+    email = models.EmailField(null=False, blank=False, max_length=100)
+    body = models.TextField(null=False, blank=False, max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=7, default=STATUS_CHOICES[0][0])
+
+    def ___str__(self):
+        return f'{self.name}-{self.created_at}'
+
+
